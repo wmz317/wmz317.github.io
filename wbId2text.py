b1dff378f72bd7d733b75be70ce7df98030bfdd8
wmz317 <68525791+wmz317@users.noreply.github.com>
2020-07-21 17:44:59 +0800

    Update wbId2text.py

--- wbId2text.py
+++ wbId2text.py
@@ -3,3 +3,6 @@ from requests import get
 url='https://m.weibo.cn/statuses/extend?id=4529065535736168'
 jsn = get(url).text
 print(jsn)
+
+#https://www.zhihu.com/question/26921730/answer/408447048
+#
