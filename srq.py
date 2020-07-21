8b07fdeb47bbb5349405ccc17b871252f3307bd3
wmz317 <68525791+wmz317@users.noreply.github.com>
2020-07-21 11:03:48 +0800

    Create srq.py

---
+++ srq.py
@@ -0,0 +1,34 @@
+from requests import get
+# import requests
+import time
+
+srq("sh510500")
+ssrq()
+
+def ssrq():
+    a500='sh510500'
+    a300='sh510300'
+    cy50='sz159949'
+    ic = 'sz159995'
+    lsts=[a300,a500,cy50,ic]
+    nw= time.strftime('%m/%d %H:%M:%S ',time.localtime(time.time()))
+    print("300+500+C50+IC"+" | "+nw)
+    for i in lsts: srq(i)
+
+
+def srq(s):
+#预留,判断SH SZ
+   
+    ht='http://hq.sinajs.cn/list='
+    ht+=s
+    r= get(ht).text
+    #r= requests.get(ht).text
+    lst= r.split(',')
+    open=lst[1]
+    yest=lst[2]
+    now= lst[3]
+    max= lst[4]
+    min= lst[5]
+    ratio = int((float(now)/float(yest)-1)*10000)/100
+    t=s+'== now:'+now+';max:'+max+'; min:'+min+ "; r:"+str(ratio)
+    return print(t)
