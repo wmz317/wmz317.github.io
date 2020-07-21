19f481346be903300f11ad09f2a25cce73f1f2b2
wmz317 <68525791+wmz317@users.noreply.github.com>
2020-07-21 22:24:44 +0800

    Update wbId2text.py

--- wbId2text.py
+++ wbId2text.py
@@ -1,8 +1,26 @@
 from requests import get
+import json
 
-url='https://m.weibo.cn/statuses/extend?id=4529065535736168'
-jsn = get(url).text
-print(jsn)
+def wbId2Text1(id='4529065535736168'):
+    id=str(id)
+    url='https://m.weibo.cn/statuses/extend?id='
+    url+=id
+    jsn = get(url).text
+    try:
+        text= jsn.encode('latin-1').decode('unicode_escape')
+        txt=text.split('"')[9].split("<br \/><br \/>")
+    except UnicodeEncodeError:
+        #print("编码异常")
+        txt=["此条编码异常..."]
+    except:
+        print("编码或未知异常")
+        
+    
+    for i in txt: print('>>'+i)
+    print('**************分隔符******************')
 
-#https://www.zhihu.com/question/26921730/answer/408447048
-#
+
+#wbId2Text1()
+# 特例故障u1='https://m.weibo.cn/statuses/extend?id=4529178461872473'
+# 正常例子 url='https://m.weibo.cn/statuses/extend?id=4529065535736168'
+
