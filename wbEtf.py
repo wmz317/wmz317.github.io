1c2f7a433ed578c74a98e064fba0ccd295abb22a
wmz317 <68525791+wmz317@users.noreply.github.com>
2020-07-22 11:12:31 +0800

    Update wbEtf.py

--- wbEtf.py
+++ wbEtf.py
@@ -1,6 +1,6 @@
-import requests
 from urllib.parse import urlencode
-from pyquery import PyQuery as pq
+from requests import get
+import json
 
 base_url = 'https://m.weibo.cn/api/container/getIndex?'
 headers = {
@@ -21,11 +21,12 @@ def get_page(page):
     }
     url = base_url + urlencode(params) #合成完整的URL
     try:
-        response = requests.get(url, headers=headers)
+        response = get(url, headers=headers)
         if response.status_code == 200: #判断响应的状态码
             return response.json(), page
     except requests.ConnectionError as e:
         print('Error', e.args)
+
 #解析并提取信息
 def parse_page(json, page: int):
     if json:
@@ -36,20 +37,36 @@ def parse_page(json, page: int):
             else:
                 item = item.get('mblog', {})
                 weibo = {}
-                weibo['id'] = item.get('id')
-                weibo['创建'] =item.get('created_at')
-                weibo['正文'] = pq(item.get('text')).text() #借助pyquery去掉正文中的HTML
-                #weibo['点赞'] = item.get('attitudes_count')
-                #weibo['评论'] = item.get('comments_count')
-                #weibo['转发'] = item.get('reposts_count')
+                weibo['time'] =item.get('created_at') 
+                weibo['id'] = item.get('id')                               
+                #weibo['正文'] = pq(item.get('text')).text() #借助pyquery去掉正文中的HTML              
                 yield weibo
+
+def wbId2Text1(id='4529065535736168'):
+    id=str(id)
+    url='https://m.weibo.cn/statuses/extend?id='
+    url+=id
+    jsn = get(url).text
+    try:
+        text= jsn.encode('latin-1').decode('unicode_escape')
+        txt=text.split('"')[9].split("<br \/><br \/>")
+    except UnicodeEncodeError:
+        #print("编码异常")
+        txt=["此条编码异常..."]
+    except:
+        print("未知异常")        
+    
+    for i in txt: print('>>> '+i)
+    print('---------------------------------------------------分隔符----------------------------------------------')
+
 
 if __name__ == '__main__':
     for page in range(1, max_page + 1):
         json = get_page(page)
         results = parse_page(*json)
-       
-        #doc=open("output.txt","a",encoding='utf8')
+        #doc=open("output.txt","a",encoding='utf8')        
         for x in results:
-            print(x)
+            if x['id']!='4360655919977273':
+              print(x)
+              wbId2Text1(x['id'])
         #doc.close()  
